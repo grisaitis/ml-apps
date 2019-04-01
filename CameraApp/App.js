39d187640c1d7cc2e68d3ed1e40eed152f889c0a
William C Grisaitis <william@grisait.is>
2019-04-01 18:16:54 -0400

    Remove instructions junk

--- CameraApp/App.js
+++ CameraApp/App.js
@@ -19,13 +19,6 @@ import {
 import RNCamera from "react-native-camera";
 
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
@@ -41,7 +34,7 @@ export default class App extends Component<Props> {
       const data = this.camera.takePicture();
       console.log('Path to image: ' + data.uri);
     } catch (err) {
-      // console.log('err: ', err);
+      console.log('err: ', err);
     }
   };
 
@@ -84,11 +77,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
   preview: {
     flex: 1,
