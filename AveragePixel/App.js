11bfe2d0ab26057896defb67a2e31fdb129cf3cd
William C Grisaitis <william@grisait.is>
2019-06-27 16:16:53 -0400

    Trim even more boilerplate code

--- AveragePixel/App.js
+++ AveragePixel/App.js
@@ -20,70 +20,25 @@ console.log(RNAveragePixelLib.addEvent("william", "orlando"));
 console.log(5);
 
 export default class App extends Component {
-  state = {
-    flash: 'off',
-    zoom: 0,
-    autoFocus: 'on',
-    autoFocusPoint: {
-      normalized: { x: 0.5, y: 0.5 }, // normalized values required for autoFocusPointOfInterest
-      drawRectPosition: {
-        x: Dimensions.get('window').width * 0.5 - 32,
-        y: Dimensions.get('window').height * 0.5 - 32,
-      },
-    },
-    depth: 0,
-    type: 'back',
-    whiteBalance: 'auto',
-    ratio: '16:9',
-    recordOptions: {
-      mute: false,
-      maxDuration: 5,
-      quality: RNCamera.Constants.VideoQuality['288p'],
-    },
-    isRecording: false,
-    canDetectFaces: false,
-    canDetectText: false,
-    canDetectBarcode: false,
-    faces: [],
-    textBlocks: [],
-    barcodes: [],
-  };
-  
-  toggle = value => () => this.setState(prevState => ({ [value]: !prevState[value] }));
-
-  renderCamera() {
-    const { canDetectFaces, canDetectText, canDetectBarcode } = this.state;
-    
-    const drawFocusRingPosition = {
-      top: this.state.autoFocusPoint.drawRectPosition.y - 32,
-      left: this.state.autoFocusPoint.drawRectPosition.x - 32,
-    };
-    return (
-      <RNCamera
-        ref={ref => {
-          this.camera = ref;
-        }}
-        style={{
-          flex: 1,
-          justifyContent: 'space-between',
-        }}
-      >
-      </RNCamera>
-    );
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text>The average pixel value of the camera is: {this.averagePixelValue}</Text>
-        {this.renderCamera()}
+        <RNCamera
+          ref={ref => {this.camera = ref;}}
+          style={styles.camera}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
   container: {
     flex: 1,
     paddingTop: 10,
