0ced416d9bc239fc52b71d057e4fbe550dc292d3
William C Grisaitis <william@grisait.is>
2019-08-16 12:39:17 -0400

    Implement FileReader.prototype.readAsArrayBuffer; used for loading onnx file

--- OnnxJsApp/components/onnx_demo.js
+++ OnnxJsApp/components/onnx_demo.js
@@ -14,6 +14,45 @@ import {
 } from "onnxjs";
 
 
+// from: https://stackoverflow.com/questions/42829838/react-native-atob-btoa-not-working-without-remote-js-debugging
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+const atob = (input = '') => {
+	let str = input.replace(/=+$/, '');
+	let output = '';
+
+	if (str.length % 4 == 1) {
+		throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
+	}
+	for (let bc = 0, bs = 0, buffer, i = 0;
+		buffer = str.charAt(i++);
+
+		~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+			bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+	) {
+		buffer = chars.indexOf(buffer);
+	}
+
+	return output;
+}
+
+FileReader.prototype.readAsArrayBuffer = function (blob) {
+	if (this.readyState === this.LOADING) throw new Error("InvalidStateError");
+	this._setReadyState(this.LOADING);
+	this._result = null;
+	this._error = null;
+	const fr = new FileReader();
+	fr.onloadend = () => {
+		const content = atob(fr.result.substr("data:application/octet-stream;base64,".length));
+		const buffer = new ArrayBuffer(content.length);
+		const view = new Uint8Array(buffer);
+		view.set(Array.from(content).map(c => c.charCodeAt(0)));
+		this._result = buffer;
+		this._setReadyState(this.DONE);
+	};
+	fr.readAsDataURL(blob);
+}
+
+
 export class OnnxDemo extends React.Component {
   runExample1(stuff) {
     console.log(stuff);
@@ -33,7 +72,11 @@ export class OnnxDemo extends React.Component {
     const session = new InferenceSession({backendHint: 'cpu'});
     console.log(session);
 
-    session.loadModel("add.onnx").then(() => {
+    // session.loadModel("add.onnx").then(() => {
+    const url_add = "https://raw.githubusercontent.com/microsoft/onnxjs/6ccc22568f7b9e712cf4bb803cf4b201f6314d3b/examples/node/add/add.onnx";
+    console.log("Loading model from:")
+    console.log(url_add)
+    session.loadModel(url_add).then(() => {
       // fails because FileReader.readAsArrayBuffer is not implemented
       console.log("Loaded model...");
       console.log(session);
